7dd6656bcf1fa3352cdc53942240b0bd2953f81c
DJ2582 <141388109+DJ2582@users.noreply.github.com>
2023-10-16 01:26:11 +0530

    Update student.c

--- student.c
+++ student.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
+// #include "structures.c"
 #include <sys/socket.h>
-void handle_student(int client_socket) {
-    // Implement student-specific functionality here
-    char message[] = "Welcome, student!\n";
-    send(client_socket, message, sizeof(message), 0);
+#include <fcntl.h>
+#include<string.h>
+#include <sys/stat.h>
+#include<stdbool.h>
+bool login_student(int client_socket,int id,char* password)
+{
+    // printf("%d",id);
+    // printf("%s",password);
+    int file=open("student.txt",O_RDONLY);
+    struct StudentDetail log_student;
+    ssize_t read_result;
+    int found=0;
 
-    // Additional interactions with the client using client_socket...
+    //reading file and checking if id and passwords match
+    while(read(file,&log_student,sizeof(struct StudentDetail)))
+    {
+        if(log_student.id==id)
+        {
+           if(strcmp(log_student.password,password)==0)
+           {
+            found=1;
+            break;
+           }
+        }
+    }
+    close(file);
+    return found;    
 }
