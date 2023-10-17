cb23f1ba137cf3a34f5978b6a40e137555ef85a1
DJ2582 <141388109+DJ2582@users.noreply.github.com>
2023-10-17 16:46:54 +0530

    Update student.c

--- student.c
+++ student.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-// #include "structures.c"
 #include <sys/socket.h>
 #include <fcntl.h>
 #include<string.h>
@@ -62,7 +61,7 @@ void view_enrolled_course(int client_socket,int id)
     // printf("after 1\n");
     while(read(fd,&course,sizeof(course)))
     {
-        if(course.student_id==id)
+        if(course.student_id==id && course.enrolled==1)
         {
             send(client_socket,&ack,sizeof(ack),0);
             send(client_socket,&course,sizeof(course),0);
@@ -74,61 +73,132 @@ void view_enrolled_course(int client_socket,int id)
 
 void enroll_course(int client_socket,int student_id,int course_id)
 {
-    //open faculty file 
    int fd=open("faculty.txt",O_RDONLY);
-    
-   //open course file
    int fd2=open("course.txt",O_RDWR);
-
-   //open enrolled courses file
-   int fd3=open("enrolled_courses.txt",O_RDWR);
-
+   int fd3=open("enrolled_courses.txt",O_RDWR,0777);
+   int flag=0;
    struct CourseDetail course;
-
-   while(read(fd2,&course,sizeof(struct CourseDetail)))
-   {
+   struct course_enrolled course_enr;  
+ while(read(fd2,&course,sizeof(struct CourseDetail)))
+ {
     if(course.id==course_id)
     {
-        if(course.seat>0)
+      while(read(fd3,&course_enr,sizeof(course_enr)))
+      {
+        if(course_enr.student_id==student_id&&course_enr.course_id==course_id)
         {
+           if(course.seat>0)
+           {
+            flag=1;
             lseek(fd2,-(sizeof(struct CourseDetail)),SEEK_CUR);
             struct CourseDetail course_copy;
-            // printf("fetch course id:%d\n",course_copy.id);
-            // printf("fetch course department:%s\n",course_copy.department);
-            // printf("fetch course name:%s\n",course_copy.name);
-            // printf("fetch course seat:%d\n",course_copy.seat);
-
             course_copy.id=course_id;
             course_copy.faculty_id=course.faculty_id;
             strcpy(course_copy.name,course.name);
             strcpy(course_copy.department,course.department);
             course_copy.seat=course.seat-1;
+            course_copy.max_seats=course.max_seats;
             course_copy.credit=course.credit;
             write(fd2,&course_copy,sizeof(struct CourseDetail));
-            send(client_socket,"Successfully Enrolled\n",sizeof("Successfully Enrolled"),0);
-            printf("available seats:%d\n",course_copy.seat);
-
+            send(client_socket,"Successfully Enrolled\n",sizeof("Successfully Enrolled\n"),0);
+            lseek(fd3,-(sizeof(struct course_enrolled)),SEEK_CUR);
             struct course_enrolled course_en;
             course_en.student_id=student_id;
             course_en.course_id=course_id;
+            course_en.enrolled=1;
             strcpy(course_en.course_name,course.name);
-            course_en.faculty_id=course.faculty_id;
+            course_en.faculty_id=course.faculty_id;     
+            write(fd3,&course_en,sizeof(struct course_enrolled));           
+           }
+           else
+           {
+            send(client_socket,"Sorry No Seats Available\n",sizeof("Sorry No Seats Available\n"),0);
+           }
+        }
+        
+      } 
 
-            printf("course name:%s\n",course_en.course_name);
-            printf("course id:%d\n",course_en.course_id);
-            printf("student id:%d\n",course_en.student_id);
-            printf("faculty id:%d\n",course_en.faculty_id);
-            lseek(fd3,0,SEEK_END);
-            write(fd3,&course_en,sizeof(struct course_enrolled));
-            
-        }
-        else
-        {
-            send(client_socket,"Sorry No Seats Available.....\n",sizeof("Sorry No Seats Available...Try Later Sometime"),0);
-        }
+      if(course.seat>0&&flag==0)
+      {
+        lseek(fd2,-(sizeof(struct CourseDetail)),SEEK_CUR);
+        struct CourseDetail course_copy;
+        course_copy.id=course_id;
+        course_copy.faculty_id=course.faculty_id;
+        strcpy(course_copy.name,course.name);
+        strcpy(course_copy.department,course.department);
+        course_copy.seat=course.seat-1;
+        course_copy.max_seats=course.max_seats;
+        course_copy.credit=course.credit;
+        write(fd2,&course_copy,sizeof(struct CourseDetail));
+        send(client_socket,"Successfully Enrolled\n",strlen("Successfully Enrolled\n"),0);
+        lseek(fd3,0,SEEK_END);
+        struct course_enrolled course_en;
+        course_en.student_id=student_id;
+        course_en.course_id=course_id;
+        course_en.enrolled=1;
+        strcpy(course_en.course_name,course.name);
+        course_en.faculty_id=course.faculty_id;
+        write(fd3,&course_en,sizeof(struct course_enrolled));           
+     }
+     else
+     {
+        send(client_socket,"Sorry No Seats Available\n",strlen("Sorry No Seats Available\n"),0);
+     }
+    }      
+ }
+  close(fd);
+  close(fd2);
+  close(fd3);
+}
+
+void drop_course(int client_socket,int student_id,int course_id)
+{
+    int fd=open("faculty.txt",O_RDONLY);
+    int fd2=open("course.txt",O_RDWR);
+    int fd3=open("enrolled_courses.txt",O_RDWR);
+    if(fd==-1||fd2==-1||fd3==-1)
+    {
+        perror("ERROR:");
     }
-  }
-
+    struct CourseDetail course;
+    struct course_enrolled course_enr;
+    while((read(fd2,&course,sizeof(struct CourseDetail))))
+    {        
+     if(course.id==course_id)
+     {
+        while((read(fd3,&course_enr,sizeof(struct course_enrolled))))
+        {
+          if(course_enr.student_id==student_id&&course_enr.course_id==course_id)
+          {
+            if(course.seat<course.max_seats)
+            {   
+             lseek(fd2,-(sizeof(struct CourseDetail)),SEEK_CUR);
+             struct CourseDetail course_copy;
+             course_copy.id=course_id;
+             course_copy.faculty_id=course.faculty_id;
+             strcpy(course_copy.name,course.name);
+             strcpy(course_copy.department,course.department);
+             course_copy.seat=course.seat+1;
+             course_copy.max_seats=course.max_seats;
+             course_copy.credit=course.credit;
+             write(fd2,&course_copy,sizeof(struct CourseDetail));
+             lseek(fd3,-(sizeof(struct course_enrolled)),SEEK_CUR);
+             struct course_enrolled course_en;
+             course_en.student_id=student_id;
+             course_en.course_id=course_id;
+             strcpy(course_en.course_name,course.name);
+             course_en.faculty_id=course.faculty_id;
+             course_en.enrolled=0;
+             write(fd3,&course_en,sizeof(struct course_enrolled));    
+            }
+            // else
+            // {
+            // send(client_socket,"not allowed",strlen("not allowed"),0);
+            // }
+          }
+        }   
+    }  
+  }    
 }
 
 void change_password_student(int client_socket,int id,char* password)
@@ -153,3 +223,4 @@ void change_password_student(int client_socket,int id,char* password)
         }   
     }
 }
+
